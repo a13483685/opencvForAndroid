da2903622dce46a298080dabfcaf3dae77bd519d
Your Name <you@example.com>
2018-06-22 22:32:16 +0800

    使用Cmake替代Android.mk来构建so

--- app/src/jni/haar_detect.cpp
+++ app/src/jni/haar_detect.cpp
@@ -35,12 +35,12 @@ extern"C" {
         cvtColor(mRgb, gray, COLOR_BGR2GRAY);
         vector<Rect> faces;
         LOGD( "This is a number from JNI: %d", flag*2);
-        face_detector.detectMultiScale(gray, faces, 1.1, 1, 0, Size(50, 50), Size(300, 300));
+//        face_detector.detectMultiScale(gray, faces, 1.1, 1, 0, Size(50, 50), Size(300, 300));
         LOGD( "This is a number from JNI: %d", flag*3);
-        if(faces.empty()) return;
-        for (int i = 0; i < faces.size(); i++) {
-            rectangle(mRgb, faces[i], Scalar(255, 0, 0), 2, 8, 0);
+//        if(faces.empty()) return;
+//        for (int i = 0; i < faces.size(); i++) {
+//            rectangle(mRgb, faces[i], Scalar(255, 0, 0), 2, 8, 0);
             LOGD( "Face Detection : %s", "Found Face");
-        }
+//        }
     }
 }
--- openCVLibrary340/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary340/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: E:\\2018\\mySelf\\opencvForAndroid\\openCVLibrary340\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: F:\\work\\android\\opencvForAndroid\\openCVLibrary340\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
